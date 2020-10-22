934ce6861b784dde407374357226ca5182b2131f
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-10-22 14:59:16 +0600

    Add files via upload

---
+++ react_frontend/build/precache-manifest.9c577b7224d1581724e4f3356639e0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971ab89af4a21ffec706a246aadd6209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be5245045f4fa084c21",
+    "url": "/static/css/main.6b09495b.chunk.css"
+  },
+  {
+    "revision": "2e87c70b42d2221ca4e9",
+    "url": "/static/js/2.301db723.chunk.js"
+  },
+  {
+    "revision": "b952d6d32f30bab0684e61579c5d99b7",
+    "url": "/static/js/2.301db723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be5245045f4fa084c21",
+    "url": "/static/js/main.4b87aa4e.chunk.js"
+  },
+  {
+    "revision": "4498116d70cc215e67db",
+    "url": "/static/js/runtime-main.75cd8b97.js"
+  }
+]);
